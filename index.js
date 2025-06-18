685c66925c41ae48f39343d504358c6fd106a749
Rajat yadav <mbmrajatit@gamil.com>
2025-06-18 20:36:49 +0530

    small adjustment

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ document.getElementById("button").addEventListener("click",()=>{
     let theme = document.body.getAttribute("data-theme");
     if(theme === "light"){
         document.body.setAttribute("data-theme","dark");
-        document.getElementById("button").textContent = "Dark";
+        document.getElementById("button").textContent = "NIGHT";
     }
     else{
         document.body.setAttribute("data-theme","light");
-        document.getElementById("button").textContent = "Light";
+        document.getElementById("button").textContent = "DAY";
     }
     
 })
